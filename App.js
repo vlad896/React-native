d520345343a5b5575e510dc675d97cd1eaa79d6b
Vladislav Fedorovich <vlados6675041@gmail.com>
2024-01-24 16:22:00 +0300

    add confirm alert when deleting

--- App.js
+++ App.js
@@ -1,61 +1,91 @@
+import { StyleSheet, View, Alert } from "react-native";
+import { Navbar } from "./app/components/Navbar";
 
-import { StyleSheet, View } from 'react-native';
-import { Navbar } from './app/components/Navbar';
+import { useState } from "react";
 
-import { useState } from 'react';
-
-import { MainScreen } from './app/screens/MainScreen';
-import { TodoScreen } from './app/screens/TodoScreen';
+import { MainScreen } from "./app/screens/MainScreen";
+import { TodoScreen } from "./app/screens/TodoScreen";
 
 export default function App() {
-	const [todos, setTodos] = useState([
-		{ id: '1', title: 'Выучить React Native' },
-		{ id: '2', title: 'Написать приложение' }
-	])
-	const [todoId, setTodoId] = useState('2')
+  const [todos, setTodos] = useState([
+    { id: "1", title: "Выучить React Native" },
+    { id: "2", title: "Написать приложение" },
+  ]);
+  const [todoId, setTodoId] = useState(null);
 
-	const addTodo = (title) => {
-		// const newTodo = {
-		// 	id: Date.now().toString(),
-		// 	title: title
-		// }
+  const addTodo = (title) => {
+    // const newTodo = {
+    // 	id: Date.now().toString(),
+    // 	title: title
+    // }
 
-		// setTodos((preTodos) => {
-		// 	return [
-		// 		...preTodos,
-		// 		newTodo
-		// 	]
-		// })
+    // setTodos((preTodos) => {
+    // 	return [
+    // 		...preTodos,
+    // 		newTodo
+    // 	]
+    // })
 
-		setTodos(prev => [...prev, {
-			id: Date.now().toString(),
-			title: title
-		}])
-	}
+    setTodos((prev) => [
+      ...prev,
+      {
+        id: Date.now().toString(),
+        title: title,
+      },
+    ]);
+  };
 
-	const removeTodo = id => {
-		setTodos(prev => prev.filter(todo => todo.id !== id))
-	}
+  const removeTodo = (id) => {
+    const todo = todos.find((t) => t.id === id);
+    Alert.alert(
+      "Удаление элемента",
+      `Вы уверены что ходите удалить "${todo.title}"`,
+      [
+        {
+          text: "Cancel",
+          style: "cancel",
+        },
+        {
+          text: "Удаление",
+          style: "destructive",
+          onPress: () => {
+            setTodoId(null);
+            setTodos((prev) => prev.filter((todo) => todo.id !== id));
+          },
+        },
+      ],
+      { cancelable: false }
+    );
+  };
 
-
-	return (
-		<View>
-			<Navbar title='ToDo App' />
-			<View style={styles.container}>
-				{todoId !== null
-					? <TodoScreen goBack={() => setTodoId(null)} onRemove={removeTodo} todo={todos.find(todo => todo.id === todoId)} />
-					: <MainScreen onSubmit={addTodo} todos={todos} onRemove={removeTodo} onOpen={(id) => {
-						setTodoId(id)
-					}} />}
-			</View>
-		</View>
-	);
+  return (
+    <View>
+      <Navbar title="ToDo App" />
+      <View style={styles.container}>
+        {todoId !== null ? (
+          <TodoScreen
+            goBack={() => setTodoId(null)}
+            onRemove={removeTodo}
+            todo={todos.find((todo) => todo.id === todoId)}
+          />
+        ) : (
+          <MainScreen
+            onSubmit={addTodo}
+            todos={todos}
+            onRemove={removeTodo}
+            onOpen={(id) => {
+              setTodoId(id);
+            }}
+          />
+        )}
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-	container: {
-		paddingHorizontal: 30,
-		paddingVertical: 20
-	}
-}
-);
+  container: {
+    paddingHorizontal: 30,
+    paddingVertical: 20,
+  },
+});
--- app/screens/TodoScreen.js
+++ app/screens/TodoScreen.js
@@ -1,39 +1,43 @@
-import React from 'react'
-import { StyleSheet, View, Text, Button } from 'react-native'
-import { THEME } from '../theme'
-import { AppCard } from '../ui/AppCard'
+import React from "react";
+import { StyleSheet, View, Text, Button } from "react-native";
+import { THEME } from "../theme";
+import { AppCard } from "../ui/AppCard";
 export const TodoScreen = ({ goBack, todo, onRemove }) => {
-	return (
-		<View>
-			<AppCard style={styles.card}>
-				<Text style={styles.title}>{todo.title}</Text>
-				<Button title='Редактировать' />
-			</AppCard>
-			<View style={styles.buttonContainer} >
-				<View style={styles.button}>
-					<Button title='Назад' onPress={goBack} />
-				</View>
-				<View style={styles.button}>
-					<Button title='Удалить' color={THEME.DANGER_COLOR} />
-				</View>
-			</View>
-		</View>
-	)
-}
+  return (
+    <View>
+      <AppCard style={styles.card}>
+        <Text style={styles.title}>{todo.title}</Text>
+        <Button title="Редактировать" />
+      </AppCard>
+      <View style={styles.buttonContainer}>
+        <View style={styles.button}>
+          <Button title="Назад" onPress={goBack} />
+        </View>
+        <View style={styles.button}>
+          <Button
+            title="Удалить"
+            onPress={() => onRemove(todo.id)}
+            color={THEME.DANGER_COLOR}
+          />
+        </View>
+      </View>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-	buttonContainer: {
-		flexDirection: 'row',
-		justifyContent: 'space-around',
-	},
-	button: {
-		width: '40%'
-	},
-	title: {
-		fontSize: 20,
-	},
-	card: {
-		marginBottom: 20,
-		padding: 15
-	}
-})
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+  },
+  button: {
+    width: "40%",
+  },
+  title: {
+    fontSize: 20,
+  },
+  card: {
+    marginBottom: 20,
+    padding: 15,
+  },
+});
