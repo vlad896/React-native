9989b0acf5b63247704b86789f0ddc0557966f08
Vladislav Fedorovich <vlados6675041@gmail.com>
2024-01-26 17:44:10 +0300

    add no-items.png

--- app/screens/MainScreen.js
+++ app/screens/MainScreen.js
@@ -1,24 +1,39 @@
-import React from 'react'
-import { View, StyleSheet, FlatList } from 'react-native';
-import { AddTodo } from '../components/AddTodo';
-import { Todo } from '../components/Todo';
+import React from "react";
+import { View, StyleSheet, FlatList, Text, Image } from "react-native";
+import { AddTodo } from "../components/AddTodo";
+import { Todo } from "../components/Todo";
 
 export const MainScreen = ({ onSubmit, onRemove, todos, onOpen }) => {
 	return (
 		<View>
 			<AddTodo onSubmit={onSubmit} />
-
-			<FlatList
-				keyExtractor={item => item.id.toString()}
-				data={todos}
-				renderItem={({ item }) => (<Todo todo={item} onRemove={onRemove} onOpen={onOpen} />)
-
-				}
-			/>
+			{todos.length !== 0 ? (
+				<FlatList
+					keyExtractor={(item) => item.id.toString()}
+					data={todos}
+					renderItem={({ item }) => (
+						<Todo todo={item} onRemove={onRemove} onOpen={onOpen} />
+					)}
+				/>
+			) : (
+				<View style={styles.ImageW}>
+					<Image style={styles.image} source={require('../../assets/no-items.png')} />
+				</View>
+			)}
 		</View>
-	)
-}
+	);
+};
 
 const styles = StyleSheet.create({
-
-})
+	ImageW: {
+		alignItems: 'center',
+		justifyContent: "center",
+		padding: 10,
+		height: 300,
+	},
+	image: {
+		width: '100%',
+		height: "100%",
+		resizeMode: 'contain',
+	}
+});
